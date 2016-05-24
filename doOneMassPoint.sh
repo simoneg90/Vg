b6687c9af0d879cd5ec53c10f274e1b652662f77
Lesya Shchutska <lesya.shchutska@cern.ch>
2016-05-24 05:31:43 -0400

    updated bias studies and datacards

--- doOneMassPoint.sh
+++ doOneMassPoint.sh
@@ -4,6 +4,11 @@ rebin=10
 debug=0
 m=$1
 
+models=(
+    "env_pdf_0_13TeV_dijet2" "env_pdf_0_13TeV_exp1" "env_pdf_0_13TeV_expow1" "env_pdf_0_13TeV_pow1" "env_pdf_0_13TeV_lau1" "env_pdf_0_13TeV_atlas1" "env_pdf_0_13TeV_vvdijet1"
+)
+
+
 ./buildInputs.sh $rebin $m 0
 ./buildDatacards.sh $m
 ./runLimits.sh $debug $m
